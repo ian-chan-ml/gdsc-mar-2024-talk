cb1b32e699e196373c063fe1384b93354d071cae
quanianitis <ichan@moneylion.com>
2024-03-27 12:41:21 +0800

    feat: use gin instead

--- demo/partial/main.go
+++ demo/partial/main.go
@@ -1,25 +1,11 @@
-// Copyright The OpenTelemetry Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
 	"fmt"
 	"math/rand"
-	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
@@ -36,19 +22,13 @@ func NewServer() *Server {
 	}
 }
 
-func (s *Server) rolldice(w http.ResponseWriter, _ *http.Request) {
+func (s *Server) rolldice(c *gin.Context) {
 	n := s.rand.Intn(6) + 1
 	logger.Info("rolldice called", zap.Int("dice", n))
-	fmt.Fprintf(w, "%v", n)
+	c.String(200, fmt.Sprintf("%v", n))
 }
 
 var logger *zap.Logger
-
-func setupHandler(s *Server) *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/rolldice", s.rolldice)
-	return mux
-}
 
 func main() {
 	var err error
@@ -61,8 +41,10 @@ func main() {
 	logger.Info("starting http server", zap.String("port", port))
 
 	s := NewServer()
-	mux := setupHandler(s)
-	if err := http.ListenAndServe(port, mux); err != nil {
+	r := gin.Default()
+	r.GET("/rolldice", s.rolldice)
+
+	if err := r.Run(port); err != nil {
 		logger.Error("error running server", zap.Error(err))
 	}
 }
